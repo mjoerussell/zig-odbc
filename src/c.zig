987076d6542f05408eb663c3c781d379506a36d7
Mitchell Russell <mitchellrussell@discover.com>
2021-05-18 14:37:12 -0500

    Don't try to import windows.h on non-windows systems

--- src/c.zig
+++ src/c.zig
@@ -1,6 +1,23 @@
-pub usingnamespace @cImport({
-    @cInclude("windows.h");
-    @cInclude("sql.h");
-    @cInclude("sqltypes.h");
-    @cInclude("sqlext.h");
-});
+// const std = @import("std");
+const builtin = @import("std").builtin;
+
+const is_windows = comptime builtin.os.tag == .windows;
+
+pub usingnamespace if (is_windows)
+    @cImport({
+        @cInclude("windows.h");
+        @cInclude("sql.h");
+        @cInclude("sqltypes.h");
+        @cInclude("sqlext.h");
+    })
+else
+    struct {
+        pub usingnamespace @cImport({
+            @cInclude("sql.h");
+            @cInclude("sqltypes.h");
+            @cInclude("sqlext.h");
+        });
+
+        pub const SQL_CP_DRIVER_AWARE = 3;
+
+    };
