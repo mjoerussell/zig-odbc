bc1ebc5a7cd4b8efc1473e2cb11684d69d24666e
Mitch <mjoerussell@gmail.com>
2021-05-19 13:21:54 -0500

    Updating cImport for windows

--- src/c.zig
+++ src/c.zig
@@ -1,23 +1,19 @@
 // const std = @import("std");
 const builtin = @import("std").builtin;
 
-const is_windows = comptime builtin.os.tag == .windows;
-
-pub usingnamespace if (is_windows)
-    @cImport({
-        @cInclude("windows.h");
-        @cInclude("sql.h");
-        @cInclude("sqltypes.h");
-        @cInclude("sqlext.h");
-    })
-else
-    struct {
-        pub usingnamespace @cImport({
+const c_decls =  if (builtin.os.tag == .windows)
+        @cImport({
+            @cInclude("windows.h");
             @cInclude("sql.h");
             @cInclude("sqltypes.h");
             @cInclude("sqlext.h");
+        })
+    else
+        @cImport({
+            @cInclude("sql.h");
+            @cInclude("sqltypes.h");
+            @cInclude("sqlext.h");
+            @cDefine("SQL_CP_DRIVER_AWARE", 3);
         });
 
-        pub const SQL_CP_DRIVER_AWARE = 3;
-
-    };
+pub usingnamespace c_decls;
