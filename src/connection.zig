9af7db2c42f8eef8b8e2fe1cf39eebea6024f2c4
Mitch <mjoerussell@gmail.com>
2021-09-21 07:59:59 -0500

    Removed for Connection

--- src/connection.zig
+++ src/connection.zig
@@ -20,13 +20,11 @@ pub const Connection = struct {
     pub const InformationTypeValue = odbc.InformationTypeValue;
 
     handle: *c_void,
-    allocator: *Allocator,
 
     connected: bool = false,
 
-    pub fn init(allocator: *Allocator, environment: *Environment) ReturnError!Connection {
+    pub fn init(environment: *Environment) ReturnError!Connection {
         var result: Connection = undefined;
-        result.allocator = allocator;
         const alloc_result = c.SQLAllocHandle(@enumToInt(HandleType.Connection), environment.handle, @ptrCast([*c]?*c_void, &result.handle));
         return switch (@intToEnum(SqlReturn, alloc_result)) {
             .InvalidHandle => error.InvalidHandle,
@@ -64,7 +62,16 @@ pub const Connection = struct {
     }
 
     pub fn connectExtended(self: *Connection, connection_string: []const u8, completion: odbc.DriverCompletion) ReturnError!void {
-        const result = c.SQLDriverConnect(self.handle, null, @intToPtr([*c]u8, @ptrToInt(connection_string.ptr)), @intCast(c_short, connection_string.len), null, 0, null, @enumToInt(completion));
+        const result = c.SQLDriverConnect(
+            self.handle, 
+            null, 
+            @intToPtr([*c]u8, @ptrToInt(connection_string.ptr)), 
+            @intCast(c_short, connection_string.len), 
+            null, 
+            0, 
+            null, 
+            @enumToInt(completion)
+        );
         return switch (@intToEnum(SqlReturn, result)) {
             .Success, .SuccessWithInfo => {
                 self.connected = true;
@@ -75,12 +82,12 @@ pub const Connection = struct {
         };
     }
 
-    pub fn browseConnect(self: *Connection, partial_connection_string: []const u8) !?[]const u8 {
+    pub fn browseConnect(self: *Connection, allocator: *Allocator, partial_connection_string: []const u8) !?[]const u8 {
         // @todo Maybe make a structure for connection string info, so users don't have to build/parse them manually
         //       Probably should at least provide utilities for getting/putting information into them
         var out_string_len: c.SQLSMALLINT = 0;
-        var out_string_buffer = try self.allocator.alloc(u8, 100);
-        errdefer self.allocator.free(out_string_buffer);
+        var out_string_buffer = try allocator.alloc(u8, 100);
+        errdefer allocator.free(out_string_buffer);
 
         run_loop: while (true) {
             const result = c.SQLBrowseConnect(self.handle, partial_connection_string.ptr, partial_connection_string.len, out_string_buffer.ptr, out_string_buffer.len, &out_string_len);
@@ -89,7 +96,7 @@ pub const Connection = struct {
                     // If SQLBrowseConnect returns success, then the user passed all required info and the connection should be established.
                     // Set connected to true and return null to indicate that there is no more info needed.
                     self.connected = true;
-                    self.allocator.free(out_string_buffer);
+                    allocator.free(out_string_buffer);
                     return null;
                 },
                 .NeedsData => {
@@ -111,7 +118,7 @@ pub const Connection = struct {
                         if (e == .StringRightTrunc) {
                             // If the error that occurred was StringRightTrunc, then the out string buffer was not big enough
                             // to store the result.
-                            out_string_buffer = try self.allocator.realloc(out_string_buffer, @intCast(usize, out_string_len) + 1);
+                            out_string_buffer = try allocator.realloc(out_string_buffer, @intCast(usize, out_string_len) + 1);
                             continue :run_loop;
                         }
                     }
@@ -160,9 +167,9 @@ pub const Connection = struct {
         };
     }
 
-    pub fn getAllEnabledFunctions(self: *Connection) ![]odbc.FunctionId {
+    pub fn getAllEnabledFunctions(self: *Connection, allocator: *Allocator) ![]odbc.FunctionId {
         var result_buffer: [c.SQL_API_ODBC3_ALL_FUNCTIONS_SIZE]c.SQLUSMALLINT = undefined;
-        var result_list = std.ArrayList(odbc.FunctionId).init(self.allocator);
+        var result_list = std.ArrayList(odbc.FunctionId).init(allocator);
         const result = c.SQLGetFunctions(self.handle, c.SQL_API_ODBC3_ALL_FUNCTIONS, @ptrCast([*c]c_ushort, &result_buffer));
         switch (@intToEnum(SqlReturn, result)) {
             .Success, .SuccessWithInfo => {
@@ -180,12 +187,12 @@ pub const Connection = struct {
         }
     }
 
-    pub fn nativeSql(self: *Connection, sql_statement: []const u8) ![]const u8 {
+    pub fn nativeSql(self: *Connection, allocator: *Allocator, sql_statement: []const u8) ![]const u8 {
         var out_statement_len: c.SQLINTEGER = 0;
         // Allocate a buffer for the out string, allocate the same number of chars as the in string because it's likely to be
         // close
-        var out_statement_buffer = self.allocator.alloc(u8, sql_statement.len);
-        errdefer self.allocator.free(out_statement_buffer);
+        var out_statement_buffer = allocator.alloc(u8, sql_statement.len);
+        errdefer allocator.free(out_statement_buffer);
 
         run_loop: while (true) {
             const result = c.SQLNativeSql(self.handle, sql_statement.ptr, sql_statement.len, out_statement_buffer.ptr, out_statement_buffer.len, &out_statement_len);
@@ -199,7 +206,7 @@ pub const Connection = struct {
                     for (errors) |e| {
                         if (e == .StringRightTrunc) {
                             // If the out string was truncated, realloc the correct length and run again
-                            out_statement_buffer = try self.allocator.realloc(out_statement_buffer, @intCast(usize, out_statement_len) + 1);
+                            out_statement_buffer = try allocator.realloc(out_statement_buffer, @intCast(usize, out_statement_len) + 1);
                             continue :run_loop;
                         }
                     }
@@ -211,9 +218,9 @@ pub const Connection = struct {
         
     }
 
-    pub fn getInfo(self: *Connection, comptime info_type: InformationType) !InformationTypeValue {
-        var result_buffer = try self.allocator.alloc(u8, 200);
-        errdefer self.allocator.free(result_buffer);
+    pub fn getInfo(self: *Connection, comptime info_type: InformationType, allocator: *Allocator) !InformationTypeValue {
+        var result_buffer = try allocator.alloc(u8, 200);
+        errdefer allocator.free(result_buffer);
         
         var result_string_length: c.SQLSMALLINT = 0;
         run_loop: while (true) {
@@ -222,7 +229,7 @@ pub const Connection = struct {
                 .Success, .SuccessWithInfo => {
                     const value = info_type.getValue(result_buffer, @intCast(usize, result_string_length));
                     if (!value.isStringType()) {
-                        self.allocator.free(result_buffer);
+                        allocator.free(result_buffer);
                     }
                     return value;
                 },
@@ -233,7 +240,7 @@ pub const Connection = struct {
                     const errors = try self.getErrors(&fba.allocator);
                     for (errors) |e| {
                         if (e == .StringRightTrunc) {
-                            result_buffer = try self.allocator.realloc(result_buffer, @intCast(usize, result_string_length));
+                            result_buffer = try allocator.realloc(result_buffer, @intCast(usize, result_string_length));
                             continue :run_loop;
                         }
                     }
@@ -244,9 +251,9 @@ pub const Connection = struct {
 
     }
 
-    pub fn getAttribute(self: *Connection, comptime attribute: Attribute) !?AttributeValue {
-        var value = try self.allocator.alloc(u8, 100);
-        defer self.allocator.free(value);
+    pub fn getAttribute(self: *Connection, comptime attribute: Attribute, allocator: *Allocator) !?AttributeValue {
+        var value = try allocator.alloc(u8, 100);
+        defer allocator.free(value);
 
         var attribute_str_len: i32 = 0;
         attr_loop: while (true) {
@@ -263,7 +270,7 @@ pub const Connection = struct {
                     const errors = self.getErrors(&fba.allocator) catch return ReturnError.Error;
                     for (errors) |err| {
                         if (err == .StringRightTrunc) {
-                            value = try self.allocator.realloc(value, @intCast(usize, attribute_str_len) + 1);
+                            value = try allocator.realloc(value, @intCast(usize, attribute_str_len) + 1);
                             // Continue the outer loop to try again
                             continue :attr_loop;
                         }
@@ -301,7 +308,7 @@ pub const Connection = struct {
         return try odbc_error.getErrors(allocator, odbc.HandleType.Connection, self.handle);
     }
 
-    pub fn getDiagnosticRecords(self: *Connection) ![]odbc_error.DiagnosticRecord {
-        return try odbc_error.getDiagnosticRecords(self.allocator, odbc.HandleType.Connection, self.handle);
+    pub fn getDiagnosticRecords(self: *Connection, allocator: *Allocator) ![]odbc_error.DiagnosticRecord {
+        return try odbc_error.getDiagnosticRecords(allocator, odbc.HandleType.Connection, self.handle);
     }
 };
